20495ba038195951314b94e8e4c4234597cbc8d6
Solimia <solomiagavlisina@gmail.com>
2025-09-22 15:01:49 +0300

    корзинка part1

---
+++ Shop_mvc_pv421/Models/ConfirmDialogModel.cs
@@ -0,0 +1,10 @@
+namespace Shop_mvc_pv421.Models
+{
+    public class ConfirmDialogModel
+    {
+        public string DialogId { get; set; }
+        public string Title { get; set; }
+        public string Message { get; set; }
+}
+       
+    }
